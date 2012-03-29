946f41bc01e271d47d8456fc3bee52d86a1da65e
Apoorv Parijat <apoorvparijat@gmail.com>
2012-03-30 04:19:53 +0530

    after optimizing

--- string-similarity/Solution.java
+++ string-similarity/Solution.java
@@ -19,6 +19,7 @@ class Solution
 
 	public static int find_similar(String x, String y)
 	{
+		/*
 		int count = 0;
 		int m = x.length() < y.length() ? x.length() : y.length();
 		for(int i = 0; i < m; i++)
@@ -29,15 +30,39 @@ class Solution
 				break;
 		}
 		return count;
+		*/
+		int factor = 1;
+		return find_similar_rec(x,y,factor);
+	}
+	
+	public static int find_similar_rec(String x, String y, int start, int end)
+	{
+		int count = 0;
+		if(start <= end)
+			return count;
+			
+		if(x.substring(start, (end-start)/2) == y.substring(start,(end-start)/2))
+			count = (end-start)/2 + find_similar_rec(x,y,(end-start)/2, end);
+		else
+			count = find_similar_rec(x,y,start, (end-start)/2);
+			
+		return count;
+
 	}
 
+	//extracts suffixes and calles find_similar_rec
+	// abababa
+	// find_similar("abababa", "bababa")
+	// find_similar("abababa", "ababa")
+	// find_similar("abababa", "ababa")
+	
 	public static int suffix_similarity(String x)
 	{
 		int count = 0;
 		int m = x.length();
 		for(int i = 1; i < m; i++)
 		{
-			count += find_similar(x, x.substring(i,m));
+			count += find_similar_rec(x, x.substring(i,m), 0, (m-i));
 		}
 		return count + m;
 	}
